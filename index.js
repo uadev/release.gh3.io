6647375061950ecedf1982e9fa3602f805ad3651
Yuriy Voziy <yuretsz@gmail.com>
2014-12-23 19:22:26 +0000

    Removed bogus index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-\\curl -i  -H "Authorization: token 26931e58f8ca6ef44ee9680d7ba8f991ccc6b395"  https://api.github.com/repos/LiveOne/liveone/compare/daily_20141218...daily_20141219
-function GitHub(url, token) {
-    this.settings.url = url;
-    this.settings.token = token;
-}
